e6766a877a1850fadb743600b444caaa86d95353
luka177 <lukapanio@gmail.com>
2021-01-02 16:35:01 +0200

    It seems to work now

--- utils/lv_drv_conf.h
+++ utils/lv_drv_conf.h
@@ -247,7 +247,7 @@
  *  Linux frame buffer device (/dev/fbx)
  *-----------------------------------------*/
 #ifndef USE_FBDEV
-#  define USE_FBDEV           0
+#  define USE_FBDEV           1
 #endif
 
 #if USE_FBDEV
@@ -361,7 +361,7 @@
  * Mouse or touchpad as evdev interface (for Linux based systems)
  *------------------------------------------------*/
 #ifndef USE_EVDEV
-#  define USE_EVDEV           0
+#  define USE_EVDEV           1
 #endif
 
 #ifndef USE_BSD_EVDEV
