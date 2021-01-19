d5c091b58daf8903723d2770dfc6b04ae305b3ad
luka177 <lukapanio@gmail.com>
2021-01-19 09:54:16 +0200

    Remove useless stuff

--- gui/gui_init.c
+++ gui/gui_init.c
@@ -77,36 +77,6 @@ void gui_init(struct abm_device *device) {
 	lv_img_set_src(cursor_obj, LV_SYMBOL_FILE); //For simlicity add a built in symbol not an image
 	lv_indev_set_cursor(mouse_indev, cursor_obj); // connect the object to the driver
 
-      
-    // Create LVGL thread
-     /*Create a Tab view object*/
-    lv_obj_t *tabview;
-    tabview = lv_tabview_create(lv_scr_act(), NULL);
-
-    /*Add 3 tabs (the tabs are page (lv_page) and can be scrolled*/
-    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "Tab 1");
-    lv_obj_t *tab2 = lv_tabview_add_tab(tabview, "Tab 2");
-    lv_obj_t *tab3 = lv_tabview_add_tab(tabview, "Tab 3");
-
-
-    /*Add content to the tabs*/
-    lv_obj_t * label = lv_label_create(tab1, NULL);
-    lv_label_set_text(label, "This the first tab\n\n"
-                             "If the content\n"
-                             "of a tab\n"
-                             "become too long\n"
-                             "the it \n"
-                             "automatically\n"
-                             "become\n"
-                             "scrollable.");
-
-    label = lv_label_create(tab2, NULL);
-    lv_label_set_text(label, "Second tab");
-
-    label = lv_label_create(tab3, NULL);
-    lv_label_set_text(label, "Third tab");
-    
     thrd_create(&tick_thrd_t, (thrd_start_t)tick_thrd, NULL);
-    for(;;)
-		usleep(5000);
+
 }
