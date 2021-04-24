272673eae5ff1174deb057fc65d0881a1a429fb1
Nick (nift4) <nift4@protonmail.com>
2021-04-24 11:49:39 +0200

    partition: move from deprecated device struct to constants

--- src/partition.c
+++ src/partition.c
@@ -1,15 +1,15 @@
 #include <sys/mount.h> 
 #include <sys/stat.h>
-#include <abm_device.h>
+#include <device_config.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <gui_error.h>
 
 #include "partition.h"
 
-void mount_sdcard_meta(struct abm_device device) {
+void mount_sdcard_meta() {
     mkdir("/meta", 0700);
-    if (0 != mount(device.sd_card_meta_path, "/meta", "ext2", 0, "")) {
+    if (0 != mount(SD_META_PATH, "/meta", "ext2", 0, "")) {
         show_error("unable to mount meta\n");
         for(;;){
          usleep(5000);   
