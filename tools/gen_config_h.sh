b4b8992635a394c2119b556322dcbdff9e12f50f
Nick (nift4) <nift4@protonmail.com>
2021-04-24 11:37:07 +0200

    Add KMEMADDR

--- tools/gen_config_h.sh
+++ tools/gen_config_h.sh
@@ -1,6 +1,4 @@
 . $1
-touch $2
-$GEN_HEADER || exit 0
 # Header
 echo "#ifndef DEVICE_CONFIG_H" >> $2
 echo "#define DEVICE_CONFIG_H" >> $2
@@ -11,5 +9,6 @@ echo "#define SD_META_PATH \"$SD_META_PATH\"" >> $2
 echo "#define TOUCHSCREEN_PATH \"$TOUCHSCREEN_PATH\"" >> $2
 echo "#define SYSTEMPART \"$SYSTEMPART\"" >> $2
 echo "#define VENDORPART \"$VENDORPART\"" >> $2
+echo "#define KMEMADDR \"$KMEMADDR\"" >> $2
 # Footer
 echo "#endif" >> $2
